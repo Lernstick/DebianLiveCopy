00d022a24665440dade6022ee9d1ee2b06d4245e
Ronny Standtke <ronny.standtke@gmx.net>
2019-06-08 14:15:49 +0200

    improved PrintSelectionDialog (scrollpane, resizing, ...)

--- src/ch/fhnw/dlcopy/gui/swing/PrintSelectionDialog.java
+++ src/ch/fhnw/dlcopy/gui/swing/PrintSelectionDialog.java
@@ -2,6 +2,7 @@ package ch.fhnw.dlcopy.gui.swing;
 
 import ch.fhnw.dlcopy.DLCopy;
 import ch.fhnw.util.ProcessExecutor;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -55,9 +56,22 @@ public class PrintSelectionDialog extends javax.swing.JDialog {
 
         initComponents();
 
+        String infoText = null;
+        if (documents.size() > 1) {
+            if (type == null) {
+                infoText = DLCopy.STRINGS.getString(
+                        "Info_Several_Files_To_Print");
+            } else {
+                infoText = MessageFormat.format(DLCopy.STRINGS.getString(
+                        "Info_Several_Files_Of_Type_To_Print"), type);
+            }
+        }
+        infoLabel.setText(infoText);
+        infoLabel.setMinimumSize(infoLabel.getPreferredSize());
+
         GridBagConstraints previewConstraints = new GridBagConstraints();
         previewConstraints.anchor = GridBagConstraints.WEST;
-        previewConstraints.insets = new java.awt.Insets(3, 0, 0, 0);
+        previewConstraints.insets = new java.awt.Insets(3, 10, 0, 0);
 
         GridBagConstraints checkBoxConstraints = new GridBagConstraints();
         checkBoxConstraints.anchor = GridBagConstraints.WEST;
@@ -65,8 +79,8 @@ public class PrintSelectionDialog extends javax.swing.JDialog {
         checkBoxConstraints.weightx = 1.0;
 
         GridBagConstraints dateLabelConstraints = new GridBagConstraints();
-        dateLabelConstraints.anchor = GridBagConstraints.WEST;
-        dateLabelConstraints.insets = new java.awt.Insets(3, 10, 0, 0);
+        dateLabelConstraints.anchor = GridBagConstraints.EAST;
+        dateLabelConstraints.insets = new java.awt.Insets(3, 10, 0, 10);
         dateLabelConstraints.gridwidth = GridBagConstraints.REMAINDER;
 
         checkBoxes = new ArrayList<>();
@@ -98,19 +112,15 @@ public class PrintSelectionDialog extends javax.swing.JDialog {
             }
         });
 
-        String infoText = null;
-        if (documents.size() > 1) {
-            if (type == null) {
-                infoText = DLCopy.STRINGS.getString(
-                        "Info_Several_Files_To_Print");
-            } else {
-                infoText = MessageFormat.format(DLCopy.STRINGS.getString(
-                        "Info_Several_Files_Of_Type_To_Print"), type);
-            }
-        }
-        infoLabel.setText(infoText);
+        scrollPane.getHorizontalScrollBar().setUnitIncrement(20);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(9);
 
         pack();
+
+        // widen a bit so that we don't get a horizontal scroll bar
+        Dimension size = getSize();
+        setSize(size.width + 20, size.height + 20);
+
         setLocationRelativeTo(parent);
     }
 
@@ -150,13 +160,19 @@ public class PrintSelectionDialog extends javax.swing.JDialog {
         java.awt.GridBagConstraints gridBagConstraints;
 
         infoLabel = new javax.swing.JLabel();
+        scrollPane = new javax.swing.JScrollPane();
         checkBoxPanel = new javax.swing.JPanel();
         okButton = new javax.swing.JButton();
         cancelButton = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("ch/fhnw/dlcopy/Strings"); // NOI18N
         setTitle(bundle.getString("PrintSelectionDialog.title")); // NOI18N
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosed(java.awt.event.WindowEvent evt) {
+                formWindowClosed(evt);
+            }
+        });
         getContentPane().setLayout(new java.awt.GridBagLayout());
 
         infoLabel.setText(bundle.getString("Info_Several_Files_Of_Type_To_Print")); // NOI18N
@@ -166,13 +182,14 @@ public class PrintSelectionDialog extends javax.swing.JDialog {
         getContentPane().add(infoLabel, gridBagConstraints);
 
         checkBoxPanel.setLayout(new java.awt.GridBagLayout());
+        scrollPane.setViewportView(checkBoxPanel);
+
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
-        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 5);
-        getContentPane().add(checkBoxPanel, gridBagConstraints);
+        getContentPane().add(scrollPane, gridBagConstraints);
 
         okButton.setText(bundle.getString("OK")); // NOI18N
         okButton.addActionListener(new java.awt.event.ActionListener() {
@@ -202,15 +219,22 @@ public class PrintSelectionDialog extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void cancelButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelButtonActionPerformed
-        setVisible(false);
-        dispose();
+        exit();
     }//GEN-LAST:event_cancelButtonActionPerformed
 
     private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okButtonActionPerformed
         okPressed = true;
+        exit();
+    }//GEN-LAST:event_okButtonActionPerformed
+
+    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
+        exit();
+    }//GEN-LAST:event_formWindowClosed
+
+    private void exit() {
         setVisible(false);
         dispose();
-    }//GEN-LAST:event_okButtonActionPerformed
+    }
 
     private class PreviewAction implements ActionListener {
 
@@ -263,5 +287,6 @@ public class PrintSelectionDialog extends javax.swing.JDialog {
     private javax.swing.JPanel checkBoxPanel;
     private javax.swing.JLabel infoLabel;
     private javax.swing.JButton okButton;
+    private javax.swing.JScrollPane scrollPane;
     // End of variables declaration//GEN-END:variables
 }
