33bad279d6317bff2813423fea6607db85512438
WJ <3376@users.noreply.gitlab.fhnw.ch>
2021-10-26 21:01:29 +0200

    style(VT342105-00): Code style

--- src/ch/fhnw/dlcopy/gui/javafx/StorageMediaManagement.java
+++ src/ch/fhnw/dlcopy/gui/javafx/StorageMediaManagement.java
@@ -5,33 +5,31 @@
  */
 package ch.fhnw.dlcopy.gui.javafx;
 
-import javafx.application.Application; 
-import javafx.fxml.FXMLLoader; 
-import javafx.scene.Parent; 
-import javafx.scene.Scene; 
-import javafx.stage.Stage; 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author user
  */
 public class StorageMediaManagement extends Application{
-     
-    @Override 
-    public void start(Stage stage) throws Exception { 
-        Parent root = FXMLLoader.load(getClass().getResource("view/startscreen.fxml")); 
-        Scene scene = new Scene(root); 
-         
-        stage.setScene(scene); 
-        stage.show(); 
-    } 
- 
-    /** 
-     * @param args the command line arguments 
-     */ 
-    public static void main(String[] args) { 
-        launch(args); 
-    } 
-     
-}
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/startscreen.fxml"));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
