076aeb8860dbeefa6d00bc5fb30a36a7e754c982
Debian Live user <user@localhost.localdomain>
2021-10-29 19:05:56 +0200

    feat(VT342105-104): Detect Button in Code

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/StartscreenUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/StartscreenUI.java
@@ -3,6 +3,7 @@ package ch.fhnw.dlcopy.gui.javafx.ui;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.*;
@@ -11,6 +12,7 @@ import javafx.scene.control.*;
 public class StartscreenUI implements View{
     
     private Parent root;
+    private Button btnIsoExport;
     
     public StartscreenUI(){
         try {
@@ -19,6 +21,21 @@ public class StartscreenUI implements View{
             Logger.getLogger(StartscreenUI.class.getName()).log(Level.SEVERE, "Failed to load FXML-file!", ex);
         }
         init();
+    }
+    
+    @Override
+    public void initControls(){
+        root.getChildrenUnmodifiable().stream()
+                .filter(child -> child.getId().equals("btnIsoExport"))
+                .findFirst()
+                .ifPresent(node -> btnIsoExport=(Button)node);
+    }
+    
+    @Override
+    public void setupEventHandlers(){
+        btnIsoExport.setOnAction(event -> {
+            System.out.println("Button was pressed");
+        });
     }
 
     @Override
