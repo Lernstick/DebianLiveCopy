668480f3bc56f02f6d8d24776cd800327a786b83
lukas.gysin <lukas.gysin@students.fhnw.ch>
2021-11-08 12:58:26 +0100

    feat(VT342105-28): Add Back Navigation

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/export/ConfigUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/export/ConfigUI.java
@@ -1,13 +1,25 @@
 package ch.fhnw.dlcopy.gui.javafx.ui.export;
 
+import ch.fhnw.dlcopy.gui.javafx.ui.StartscreenUI;
 import ch.fhnw.dlcopy.gui.javafx.ui.View;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class ConfigUI extends View {
     @FXML private TextField txtDVDLabel;
+    @FXML private Button btnBack;
 
     public ConfigUI(){
         resourcePath = getClass().getResource("/fxml/export/config.fxml");
     }
+
+    @Override
+    protected void setupEventHandlers() {
+        btnBack.setOnAction(event -> {
+            context.setScene(new StartscreenUI());
+        });
+    }
+    
+    
 }
