cd7e97beddf2a4df7967653dbfd7be7cd0da544a
lukas.gysin <lukas.gysin@students.fhnw.ch>
2021-12-02 15:43:21 +0100

    style: Delete unused Imports

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/StorageMediaManagement.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/StorageMediaManagement.java
@@ -8,7 +8,6 @@ package ch.fhnw.dlcopy.gui.javafx;
 import ch.fhnw.dlcopy.Installer;
 import ch.fhnw.dlcopy.gui.javafx.ui.StartscreenUI;
 import ch.fhnw.dlcopy.gui.javafx.ui.View;
-import ch.fhnw.dlcopy.gui.javafx.ui.exportsystem.InfoUI;
 import ch.fhnw.dlcopy.model.PresentationModel;
 import ch.fhnw.filecopier.FileCopier;
 import ch.fhnw.util.StorageDevice;
