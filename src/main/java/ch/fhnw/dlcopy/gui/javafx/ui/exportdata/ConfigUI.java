56e6a6f97c4aafd1c13071a3a8cbe7981238ebd4
lukas.gysin <lukas.gysin@students.fhnw.ch>
2021-12-02 15:20:02 +0100

    feat: Resize Images on Dataexport

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/exportdata/ConfigUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/exportdata/ConfigUI.java
@@ -1,10 +1,21 @@
 package ch.fhnw.dlcopy.gui.javafx.ui.exportdata;
 
 import ch.fhnw.dlcopy.gui.javafx.ui.View;
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 
 public class ConfigUI extends View{
     
+    @FXML private ImageView imgTargetDirectory;
+    
     public ConfigUI(){
         resourcePath = getClass().getResource("/fxml/exportData/dataexport.fxml");
+    }
+    
+    @Override
+    protected void setupEventHandlers() {
+        imgTargetDirectory.fitHeightProperty().bind(Bindings.divide(model.heightProperty(), 25.714));
+        imgTargetDirectory.fitWidthProperty().bind(Bindings.divide(model.heightProperty(), 4.1739));
     }
 }
