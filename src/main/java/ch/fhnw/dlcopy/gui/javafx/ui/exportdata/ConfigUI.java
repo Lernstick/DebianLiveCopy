c3303b8ab621d0e2f90c82249ac3ab40877b859c
WJ <3376@users.noreply.gitlab.fhnw.ch>
2021-12-02 16:07:34 +0100

    feat: Added switchbutton to systemexport

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/exportdata/ConfigUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/exportdata/ConfigUI.java
@@ -2,10 +2,9 @@ package ch.fhnw.dlcopy.gui.javafx.ui.exportdata;
 
 import ch.fhnw.dlcopy.gui.javafx.ui.View;
 
-
 public class ConfigUI extends View{
     
     public ConfigUI(){
-        resourcePath = getClass().getResource("/fxml/exportData/dataexport-responsive.fxml");
+        resourcePath = getClass().getResource("/fxml/exportData/dataexport.fxml");
     }
 }
