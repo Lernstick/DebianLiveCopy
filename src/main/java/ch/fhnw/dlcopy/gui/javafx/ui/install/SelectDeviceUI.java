c326f325f03dfd384be41e91a2be1ddb73e6f85c
lukas.gysin <lukas.gysin@students.fhnw.ch>
2021-12-17 18:22:23 +0100

    feat: Display the list in the view

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/SelectDeviceUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/SelectDeviceUI.java
@@ -7,11 +7,15 @@ import java.io.IOException;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import org.freedesktop.dbus.exceptions.DBusException;
 
 public class SelectDeviceUI extends View {
     
-    private List<StorageDevice> devicesRaw;
+    private List<StorageDevice> devices;
+    
+    @FXML private ListView<StorageDevice> lvDevices;
     
     public SelectDeviceUI(){
         resourcePath = getClass().getResource("/fxml/install/selectdevice.fxml");
@@ -19,8 +23,9 @@ public class SelectDeviceUI extends View {
     
     protected void initControls(){
         try {
-            devicesRaw = DLCopy.getStorageDevices(false, false, "bootDeviceName");
-            devicesRaw.forEach(device -> {System.out.println(device);});
+            devices = DLCopy.getStorageDevices(false, false, "bootDeviceName");
+            lvDevices.getItems().clear();
+            lvDevices.getItems().addAll(devices);
         } catch (IOException | DBusException ex) {
             Logger.getLogger(SelectDeviceUI.class.getName()).log(Level.SEVERE, null, ex);
         }
