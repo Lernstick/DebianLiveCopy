8a998a64180044d1a5857789b82229337ed309e0
lukas.gysin <lukas.gysin@students.fhnw.ch>
2021-12-21 09:51:15 +0100

    feat: Update device list
    Update the installation device list, when a new device is plugged in

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/SelectDeviceUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/SelectDeviceUI.java
@@ -5,8 +5,11 @@ import ch.fhnw.dlcopy.gui.javafx.ui.View;
 import ch.fhnw.util.StorageDevice;
 import java.io.IOException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import org.freedesktop.dbus.exceptions.DBusException;
@@ -22,12 +25,22 @@ public class SelectDeviceUI extends View {
     }
     
     protected void initControls(){
-        try {
-            devices = DLCopy.getStorageDevices(false, false, "bootDeviceName");
-            lvDevices.getItems().clear();
-            lvDevices.getItems().addAll(devices);
-        } catch (IOException | DBusException ex) {
-            Logger.getLogger(SelectDeviceUI.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        TimerTask listUpdater = new TimerTask(){
+            @Override
+            public void run() {
+                try {
+                    devices = DLCopy.getStorageDevices(false, false, "bootDeviceName");
+                    Platform.runLater(() -> {
+                        lvDevices.getItems().clear();
+                        lvDevices.getItems().addAll(devices);
+                    });
+                } catch (Exception ex) {
+                    Logger.getLogger(SelectDeviceUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+        
+        Timer listUpdateTimer = new Timer();
+        listUpdateTimer.scheduleAtFixedRate(listUpdater, 0, 1000L); // Starts the `lisstUpdater`-task each 1000ms (1sec)
     }
 }
