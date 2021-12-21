5ac4e381527d7b02da57ae41c8a9ea2eb3e1682a
lukas.gysin <lukas.gysin@students.fhnw.ch>
2021-12-21 12:36:33 +0100

    feat: Keep Selection
    Keep the selected item selected

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/SelectDeviceUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/SelectDeviceUI.java
@@ -4,6 +4,7 @@ import ch.fhnw.dlcopy.DLCopy;
 import ch.fhnw.dlcopy.gui.javafx.ui.StartscreenUI;
 import ch.fhnw.dlcopy.gui.javafx.ui.View;
 import ch.fhnw.util.StorageDevice;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -16,7 +17,6 @@ import javafx.scene.control.ListView;
 
 public class SelectDeviceUI extends View {
     
-    private List<StorageDevice> devices;
     private final Timer listUpdateTimer = new Timer();
     
     @FXML private Button btnBack;
@@ -42,10 +42,24 @@ public class SelectDeviceUI extends View {
             @Override
             public void run() {
                 try {
-                    devices = DLCopy.getStorageDevices(false, false, "bootDeviceName");
+                    List<StorageDevice> pluggedDevices = DLCopy.getStorageDevices(false, false, "bootDeviceName");
+                    List<StorageDevice> removedDevices = new ArrayList<>();
+                    List<StorageDevice> addedDevices = new ArrayList<>();
+                    for (StorageDevice device : pluggedDevices) {
+                        if(!lvDevices.getItems().contains(device)){
+                            // Plugged deice is not shown yet
+                            addedDevices.add(device);
+                        }
+                    }
+                    for (StorageDevice device : lvDevices.getItems()) {
+                        if (!pluggedDevices.contains(device)) {
+                            // Shown device is not plugged anymore
+                            removedDevices.add(device);
+                        }
+                    }
                     Platform.runLater(() -> {
-                        lvDevices.getItems().clear();
-                        lvDevices.getItems().addAll(devices);
+                        lvDevices.getItems().removeAll(removedDevices);
+                        lvDevices.getItems().addAll(addedDevices);
                     });
                 } catch (Exception ex) {
                     Logger.getLogger(SelectDeviceUI.class.getName()).log(Level.SEVERE, null, ex);
