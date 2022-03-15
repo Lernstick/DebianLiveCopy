d2561d52b28991285c85250eb108402ccaa0b2e3
lukas.gysin <lukas.gysin@students.fhnw.ch>
2022-03-15 09:49:23 +0100

    feat: Show Start Time

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/InstallationReportUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/InstallationReportUI.java
@@ -3,6 +3,7 @@ package ch.fhnw.dlcopy.gui.javafx.ui.install;
 import ch.fhnw.dlcopy.StorageDeviceResult;
 import ch.fhnw.dlcopy.gui.javafx.ui.View;
 import ch.fhnw.util.StorageDevice;
+import java.time.format.DateTimeFormatter;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
@@ -15,6 +16,7 @@ public class InstallationReportUI extends View{
     @FXML TableColumn<StorageDeviceResult, String> colMountpoint;
     @FXML TableColumn<StorageDeviceResult, String> colSerial;
     @FXML TableColumn<StorageDeviceResult, String> colSize;
+    @FXML TableColumn<StorageDeviceResult, String> colStart;
     @FXML TableColumn<StorageDeviceResult, String> colVendor;
     @FXML TableView<StorageDeviceResult> tvReport;
     
@@ -28,6 +30,7 @@ public class InstallationReportUI extends View{
         colMountpoint.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getStorageDevice().getFullDevice()));
         colSerial.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getStorageDevice().getSerial()));
         colSize.setCellValueFactory(cell -> new SimpleStringProperty(String.valueOf(cell.getValue().getStorageDevice().getSize())));
+        colStart.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getStartTime().format(DateTimeFormatter.ISO_LOCAL_TIME)));
         colVendor.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getStorageDevice().getVendor()));
         
         tvReport.setItems(InstallControler.getInstance().getReport());
