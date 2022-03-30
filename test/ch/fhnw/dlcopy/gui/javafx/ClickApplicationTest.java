4c223be53c4b61b238fffd6d241cdfe11e0e49c2
lukas.gysin <lukas.gysin@students.fhnw.ch>
2022-03-30 12:59:17 +0200

    feat: WIP

---
+++ test/ch/fhnw/dlcopy/gui/javafx/ClickApplicationTest.java
@@ -0,0 +1,31 @@
+package ch.fhnw.dlcopy.gui.javafx;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+// import org.
+
+import org.junit.Test;
+
+public class ClickApplicationTest { // extends ApplicationTest {
+//    @Override public void start(Stage stage) {
+//        Parent sceneRoot = new ClickApplication.ClickPane();
+//        Scene scene = new Scene(sceneRoot, 100, 100);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//    @Test public void should_contain_button() {
+//        // expect:
+//        verifyThat(".button", hasText("click me!"));
+//    }
+//
+//    @Test public void should_click_on_button() {
+//        // when:
+//        clickOn(".button");
+//
+//        // then:
+//        verifyThat(".button", hasText("clicked!"));
+//    }
+}
