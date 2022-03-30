1bb18028770aacecc7027851aa164384b046eecb
lukas.gysin <lukas.gysin@students.fhnw.ch>
2022-03-30 14:27:41 +0200

    feat: Add TestFX

--- test/ch/fhnw/dlcopy/gui/javafx/ClickApplicationTest.java
+++ test/ch/fhnw/dlcopy/gui/javafx/ClickApplicationTest.java
@@ -1,31 +1,38 @@
 package ch.fhnw.dlcopy.gui.javafx;
 
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.robot.Motion;
+
+import javafx.scene.control.Button;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-// import org.
+import org.junit.Test;
 
 import org.junit.Test;
 
-public class ClickApplicationTest { // extends ApplicationTest {
-//    @Override public void start(Stage stage) {
-//        Parent sceneRoot = new ClickApplication.ClickPane();
-//        Scene scene = new Scene(sceneRoot, 100, 100);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//    @Test public void should_contain_button() {
-//        // expect:
-//        verifyThat(".button", hasText("click me!"));
-//    }
-//
-//    @Test public void should_click_on_button() {
-//        // when:
-//        clickOn(".button");
-//
-//        // then:
-//        verifyThat(".button", hasText("clicked!"));
-//    }
+public class ClickApplicationTest extends ApplicationTest {
+    @Override public void start(Stage stage) {
+        Parent sceneRoot = new ClickApplication.ClickPane();
+        Scene scene = new Scene(sceneRoot, 100, 100);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Test public void should_contain_button() {
+        // expect:
+        verifyThat(".button", hasText("click me!"));
+    }
+
+    @Test public void should_click_on_button() {
+        // when:
+        clickOn(".button");
+
+        // then:
+        verifyThat(".button", hasText("clicked!"));
+    }
 }
