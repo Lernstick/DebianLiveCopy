8b17147bbcf47f2af9c00aa4bf38b3e892cd8a5b
David Wyss <david.wyss@students.fhnw.ch>
2022-03-30 12:42:12 +0200

    fix: Update LoadUI.java

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/LoadUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/LoadUI.java
@@ -28,7 +28,6 @@ public class LoadUI extends View {
     
     
     
-    private ResourceBundle stringBundle = ResourceBundle.getBundle("strings/Strings");
     
     private String tmpMessage;
     
