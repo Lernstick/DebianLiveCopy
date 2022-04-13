4648ec562ba3242519dd2465b7a9c4e2501250d0
lukas.gysin <lukas.gysin@students.fhnw.ch>
2022-04-13 16:32:27 +0200

    refactor: Make the DLCopyGui more usefull
    By default all methods throw a `UnsupportedOperationException()`.
    Because of this, we don't have to writte all of them in our classes.
    
    VT342105-349

--- src/main/java/ch/fhnw/dlcopy/gui/DLCopyGUI.java
+++ src/main/java/ch/fhnw/dlcopy/gui/DLCopyGUI.java
@@ -16,20 +16,26 @@ public interface DLCopyGUI {
     /**
      * shows the user interface for showing the installation progress
      */
-    public void showInstallProgress();
+    public default void showInstallProgress() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * sets the info about the currently installed device
      *
      * @param storageDevice the StorageDevice to be installed
      */
-    public void installingDeviceStarted(StorageDevice storageDevice);
+    public default void installingDeviceStarted(StorageDevice storageDevice) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for creating file systems of a running
      * installation
      */
-    public void showInstallCreatingFileSystems();
+    public default void showInstallCreatingFileSystems() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for overwriting the data partition with random
@@ -38,8 +44,10 @@ public interface DLCopyGUI {
      * @param done the amount of data that is already overwritten
      * @param size the size of the data partition
      */
-    public void showInstallOverwritingDataPartitionWithRandomData(
-            long done, long size);
+    public default void showInstallOverwritingDataPartitionWithRandomData(
+            long done, long size) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for copying files of a running installation
@@ -47,7 +55,9 @@ public interface DLCopyGUI {
      * @param fileCopier the FileCopier used for copying files of an
      * installation
      */
-    public void showInstallFileCopy(FileCopier fileCopier);
+    public default void showInstallFileCopy(FileCopier fileCopier) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for copying the persistency partition during
@@ -57,26 +67,34 @@ public interface DLCopyGUI {
      * @param copyScript the shell script for the copy operation
      * @param sourcePath the path to the source directory
      */
-    public void showInstallPersistencyCopy(
-            Installer installer, String copyScript, String sourcePath);
+    public default void showInstallPersistencyCopy(
+            Installer installer, String copyScript, String sourcePath) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * sets the current output line of the copy process
      *
      * @param line the current output line of the copy process
      */
-    public void setInstallCopyLine(String line);
+    public default void setInstallCopyLine(String line) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for unmouting file systems during installation
      */
-    public void showInstallUnmounting();
+    public default void showInstallUnmounting() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for writing the boot sector of a running
      * installation
      */
-    public void showInstallWritingBootSector();
+    public default void showInstallWritingBootSector() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * called when installing of a StorageDevice finished
@@ -85,40 +103,54 @@ public interface DLCopyGUI {
      * no error
      * @param autoNumberStart the new auto numbering start value
      */
-    public void installingDeviceFinished(
-            String errorMessage, int autoNumberStart);
+    public default void installingDeviceFinished(
+            String errorMessage, int autoNumberStart) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * called when installing all selected StorageDevices finished
      */
-    public void installingListFinished();
+    public default void installingListFinished() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for creating file systems of a running upgrade
      */
-    public void showUpgradeCreatingFileSystems();
+    public default void showUpgradeCreatingFileSystems() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for copying files of a running upgrade
      *
      * @param fileCopier the FileCopier used for copying files of an upgrade
      */
-    public void showUpgradeFileCopy(FileCopier fileCopier);
+    public default void showUpgradeFileCopy(FileCopier fileCopier) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for unmouting file systems during upgrade
      */
-    public void showUpgradeUnmounting();
+    public default void showUpgradeUnmounting() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for writing the boot sector of a running upgrade
      */
-    public void showUpgradeWritingBootSector();
+    public default void showUpgradeWritingBootSector() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for backing up during a running upgrade
      */
-    public void showUpgradeBackup();
+    public default void showUpgradeBackup() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for backing up the exchange partition during a
@@ -127,19 +159,25 @@ public interface DLCopyGUI {
      * @param fileCopier the FileCopier used for backing up the exchange
      * partition
      */
-    public void showUpgradeBackupExchangePartition(FileCopier fileCopier);
+    public default void showUpgradeBackupExchangePartition(FileCopier fileCopier) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for initializing the restore operation during a
      * running upgrade
      */
-    public void showUpgradeRestoreInit();
+    public default void showUpgradeRestoreInit() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for running the restore operation during a
      * running upgrade
      */
-    public void showUpgradeRestoreRunning();
+    public default void showUpgradeRestoreRunning() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for backing up the exchange partition during a
@@ -148,26 +186,34 @@ public interface DLCopyGUI {
      * @param fileCopier the FileCopier used for backing up the exchange
      * partition
      */
-    public void showUpgradeRestoreExchangePartition(FileCopier fileCopier);
+    public default void showUpgradeRestoreExchangePartition(FileCopier fileCopier) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for resetting the data partition up during a
      * running upgrade
      */
-    public void showUpgradeDataPartitionReset();
+    public default void showUpgradeDataPartitionReset() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for changing partition sizes up during a running
      * upgrade
      */
-    public void showUpgradeChangingPartitionSizes();
+    public default void showUpgradeChangingPartitionSizes() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * sets the progress of the backup/restore process when upgrading a system
      *
      * @param progressInfo the backup progress message (e.g. "x of y files")
      */
-    public void setUpgradeBackupProgress(String progressInfo);
+    public default void setUpgradeBackupProgress(String progressInfo) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * sets the currently processed file of the backup/restore process when
@@ -175,7 +221,9 @@ public interface DLCopyGUI {
      *
      * @param filename the name of the currently processed file
      */
-    public void setUpgradeBackupFilename(String filename);
+    public default void setUpgradeBackupFilename(String filename) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * sets the duration of the currently running backup/restore process
@@ -183,20 +231,26 @@ public interface DLCopyGUI {
      * @param duration the duration of the currently running backup/restore
      * process
      */
-    public void setUpgradeBackupDuration(long duration);
+    public default void setUpgradeBackupDuration(long duration) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for resetting the system partition up during a
      * running upgrade
      */
-    public void showUpgradeSystemPartitionReset();
+    public default void showUpgradeSystemPartitionReset() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * sets the info about the currently upgraded device
      *
      * @param storageDevice the StorageDevice to be upgraded
      */
-    public void upgradingDeviceStarted(StorageDevice storageDevice);
+    public default void upgradingDeviceStarted(StorageDevice storageDevice) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * called when upgrading of a StorageDevice finished
@@ -204,19 +258,25 @@ public interface DLCopyGUI {
      * @param errorMessage the error message or <code>null</code> if there was
      * no error
      */
-    public void upgradingDeviceFinished(String errorMessage);
+    public default void upgradingDeviceFinished(String errorMessage) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * called when upgrading all selected StorageDevices finished
      */
-    public void upgradingListFinished();
+    public default void upgradingListFinished() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows a message regarding the ISO creation progress
      *
      * @param message the message to show
      */
-    public void showIsoProgressMessage(String message);
+    public default void showIsoProgressMessage(String message) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows a message and progress percentage value regarding the ISO creation
@@ -225,7 +285,9 @@ public interface DLCopyGUI {
      * @param message the message to show
      * @param value the progress given in percent
      */
-    public void showIsoProgressMessage(String message, int value);
+    public default void showIsoProgressMessage(String message, int value) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * called when creating the ISO file finished
@@ -233,14 +295,18 @@ public interface DLCopyGUI {
      * @param path the path to the created ISO
      * @param success if creating the ISO was successfull
      */
-    public void isoCreationFinished(String path, boolean success);
+    public default void isoCreationFinished(String path, boolean success) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows a message regarding the SquashFS creation progress
      *
      * @param message the message to show
      */
-    public void showSquashFSProgressMessage(String message);
+    public default void showSquashFSProgressMessage(String message) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows a message and progress percentage value regarding the SquashFS creation
@@ -249,7 +315,9 @@ public interface DLCopyGUI {
      * @param message the message to show
      * @param value the progress given in percent
      */
-    public void showSquashFSProgressMessage(String message, int value);
+    public default void showSquashFSProgressMessage(String message, int value) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * called when creating the SquashFS file finished
@@ -257,24 +325,32 @@ public interface DLCopyGUI {
      * @param path the path to the created SquashFS
      * @param success if creating the SquashFS was successfull
      */
-    public void squashFSCreationFinished(String path, boolean success);
+    public default void squashFSCreationFinished(String path, boolean success) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for visualization of the reset progress
      */
-    public void showResetProgress();
+    public default void showResetProgress() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * sets the info about the currently reset device
      *
      * @param storageDevice the StorageDevice to be reset
      */
-    public void resettingDeviceStarted(StorageDevice storageDevice);
+    public default void resettingDeviceStarted(StorageDevice storageDevice) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for printing documents during reset
      */
-    public void showPrintingDocuments();
+    public default void showPrintingDocuments() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user a list of documents that can be selected to print
@@ -284,8 +360,10 @@ public interface DLCopyGUI {
      * @param documents the list of documents to print
      * @return the list of documents selected for printing
      */
-    public List<Path> selectDocumentsToPrint(
-            String type, String mountPath, List<Path> documents);
+    public default List<Path> selectDocumentsToPrint(
+            String type, String mountPath, List<Path> documents) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for backing up the exchange partition during a
@@ -294,23 +372,31 @@ public interface DLCopyGUI {
      * @param fileCopier the FileCopier used for backing up the exchange
      * partition
      */
-    public void showResetBackup(FileCopier fileCopier);
+    public default void showResetBackup(FileCopier fileCopier) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for formatting the exchange partition during
      * reset
      */
-    public void showResetFormattingExchangePartition();
+    public default void showResetFormattingExchangePartition() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for formatting the data partition during reset
      */
-    public void showResetFormattingDataPartition();
+    public default void showResetFormattingDataPartition() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for removing files during reset
      */
-    public void showResetRemovingFiles();
+    public default void showResetRemovingFiles() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows the user interface for restoring files during a running reset
@@ -318,21 +404,27 @@ public interface DLCopyGUI {
      * @param fileCopier the FileCopier used for restoring files during a
      * running reset
      */
-    public void showResetRestore(FileCopier fileCopier);
+    public default void showResetRestore(FileCopier fileCopier) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * called when resetting all selected StorageDevices finished
      *
      * @param success if resetting all storage devices was successful
      */
-    public void resettingFinished(boolean success);
+    public default void resettingFinished(boolean success) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows an error message
      *
      * @param errorMessage the error message
      */
-    public void showErrorMessage(String errorMessage);
+    public default void showErrorMessage(String errorMessage) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     /**
      * shows a confirm dialog
@@ -341,5 +433,7 @@ public interface DLCopyGUI {
      * @param message the message to display
      * @return true, if the user confirmed, false otherwise
      */
-    public boolean showConfirmDialog(String title, String message);
+    public default boolean showConfirmDialog(String title, String message) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
