4ae893e3755af9bf1a666e1f6420ae01550951a4
lukas.gysin <lukas.gysin@students.fhnw.ch>
2022-04-13 16:43:57 +0200

    docs: Add documentation for the `InstallController`

--- src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/InstallControler.java
+++ src/main/java/ch/fhnw/dlcopy/gui/javafx/ui/install/InstallControler.java
@@ -1,26 +1,44 @@
 package ch.fhnw.dlcopy.gui.javafx.ui.install;
 
 import ch.fhnw.dlcopy.StorageDeviceResult;
+import ch.fhnw.dlcopy.gui.DLCopyGUI;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class InstallControler {
+/**
+ * A controler for the installation path.
+ * Holds the current state of the installation.
+ * @author lukas-gysin
+ */
+public class InstallControler implements DLCopyGUI {
     
+    /**
+     * The singleton instance of the class
+     */
     private static InstallControler instance = null;
     
-    private ObservableList<StorageDeviceResult> report = FXCollections.observableArrayList();
+    /**
+     * A list of all installations (pending, ongoing, failed and succeeded)
+     */
+    private ObservableList<StorageDeviceResult> installations = FXCollections.observableArrayList();
     
+    /**
+     * Mark the constructor <c>private</c>, so it is not accessable from outside
+     */
     private InstallControler() {}
     
+    /**
+     * A thread save lacy constructor for the singleton
+     * @return The instance of the singleton
+     */
     public static synchronized InstallControler getInstance(){
         if (instance == null) {
             instance = new InstallControler();
         }
-        
         return instance;
     }
     
     public ObservableList<StorageDeviceResult> getReport() {
-        return report;
+        return installations;
     }
 }
