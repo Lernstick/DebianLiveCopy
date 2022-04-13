092a0f8aa7fc9c1b44f53b6959349cff9f57d40c
lukas.gysin <lukas.gysin@students.fhnw.ch>
2022-04-13 13:20:26 +0200

    fix: Add Mockito
    Add Mockito as a mock-Framework to avoid nullpointer exceptions.
    And also fixed the selector. -> select the id and not the class
    
    VT342105-327

--- test/ch/fhnw/dlcopy/gui/javafx/ui/StartscreenUITest.java
+++ test/ch/fhnw/dlcopy/gui/javafx/ui/StartscreenUITest.java
@@ -1,6 +1,8 @@
 
 package ch.fhnw.dlcopy.gui.javafx.ui;
 
+import ch.fhnw.dlcopy.gui.javafx.SceneContext;
+import static org.mockito.Mockito.*;
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
 
@@ -13,8 +15,10 @@ import org.testfx.framework.junit.ApplicationTest;
 public class StartscreenUITest extends ApplicationTest {
     
     @Override public void start(Stage stage) throws Exception {
+        SceneContext sceneContext = mock(SceneContext.class);
+        
         View view = new StartscreenUI();
-        Parent root = view.getRoot(null);
+        Parent root = view.getRoot(sceneContext);
         Scene scene = new Scene(root);
         stage.setScene(scene);
         
@@ -23,6 +27,6 @@ public class StartscreenUITest extends ApplicationTest {
     
     @Test public void hasAButton() {
         // expect:
-        verifyThat(".btnInstall", hasText("Installieren"));
+        verifyThat("#btnInstall", hasText("Installieren"));
     }
 }
